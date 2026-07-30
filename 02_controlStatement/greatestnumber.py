569eccbdbd0ba4fa7ff4acc9f7775be64aaf0d41
Nidhi-Maurya <mauryanidhi1610@gmail.com>
2026-07-30 23:38:22 +0530

    Day 2 conditional Statement

---
+++ 02_controlStatement/greatestnumber.py
@@ -0,0 +1,12 @@
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+n3=int(input("enter third number"))
+
+
+if n1>n2 and n1>n3:
+    print("greater number is",n1)
+elif n2>n1 and n2>n3:
+    print("greater number is",n2)
+else:
+    print("greater number is",n3)
+ 
